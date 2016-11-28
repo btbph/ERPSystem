4f37cebc0d0a213a2e17531d9e8363eda0a09eeb
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2016-11-28 10:38:43 +0300

    Added Classes

---
+++ BankDocument.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mikhail Ulyanov on 28.11.2016.
+//
+
+#include "BankDocument.h"
---
+++ WarehouseDocument.h
@@ -0,0 +1,14 @@
+//
+// Created by Mikhail Ulyanov on 28.11.2016.
+//
+
+#ifndef ERPSYSTEM_WAREHOUSEDOCUMENT_H
+#define ERPSYSTEM_WAREHOUSEDOCUMENT_H
+
+
+class WarehouseDocument {
+
+};
+
+
+#endif //ERPSYSTEM_WAREHOUSEDOCUMENT_H
